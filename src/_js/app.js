5e4cec2e3e099d91d8d00e104ec6ba47b05f3b85
Dominic Blais <dom@Dominics-MacBook-Pro.local>
2017-06-25 07:39:40 -0700

    Relocated subdirectories. Added angular and bootstrap hookups.

---
+++ src/_js/app.js
@@ -0,0 +1,13 @@
+/* GitHub Pull Explorer Application Code
+   https://github.com/DominicBlais/pullexplorer for more info
+
+   Notes:
+     TBD
+ */
+
+angular.module('pullorerApp', []).controller('PullorerController', ['$scope',
+    function ($scope) {
+
+    }
+]);
+
